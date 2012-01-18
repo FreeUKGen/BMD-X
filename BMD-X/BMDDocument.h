30a8bdd6c2f7ce8e6e2fe8be70c4d0d37f508bb7
Steven Fuchs <stwf@yahoo.com>
2012-01-18 07:09:38 -0500

    Initial Commit

---
+++ BMD-X/BMDDocument.h
@@ -0,0 +1,15 @@
+//
+//  BMDDocument.h
+//  BMD-X
+//
+//  Created by Steven Fuchs on 1/18/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BMDDocument : NSDocument {
+@private
+}
+
+@end
---
+++ BMD-X/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BMD-X
+//
+//  Created by Steven Fuchs on 1/18/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
