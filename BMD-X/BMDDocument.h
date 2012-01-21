5e7ea56c2b0c056b8507e97bdbcf0ecb24b14fa3
Steven Fuchs <stwf@yahoo.com>
2012-01-20 23:38:31 -0500

    All dialogs work correctly, fields do nothing.

--- BMD-X/BMDDocument.h
+++ BMD-X/BMDDocument.h
@@ -9,7 +9,57 @@
 #import <Cocoa/Cocoa.h>
 
 @interface BMDDocument : NSDocument {
+    
+    IBOutlet	NSTextField*            pageFieldSolo;
+    IBOutlet	NSTextField*            yearField;
+    IBOutlet	NSTextField*			pageField;
+    IBOutlet	NSTextField*			commentField;
+    IBOutlet	NSPopUpButton*          formatMenu;
+    IBOutlet	NSPopUpButton*          quarterMenu;
+    IBOutlet	NSTextField*            rangeField;
+    IBOutlet	NSTextField*			freeBMDRefField;
+    IBOutlet	NSTextField*			locationField;
+    IBOutlet	NSDatePicker*			dateField;
+    
+    
+    IBOutlet	NSTextField*            emailField;
+    IBOutlet	NSTextField*			passwordField;
+    IBOutlet	NSPopUpButton*          orderMenu;
+    IBOutlet	NSPopUpButton*          typeMenu;
+    
+    IBOutlet	NSPanel*				mPageWindow;
+    IBOutlet	NSPanel*				mCommentWindow;
+    IBOutlet	NSPanel*				mEntryWindow;
+    IBOutlet	NSPanel*				mSourceWindow;
+    IBOutlet    NSTextView *textView;
+    
+    NSAttributedString *mString;
 @private
 }
+- (NSAttributedString *) string;
+
+- (void) setString: (NSAttributedString *) value;
+
+
+- (IBAction)sendBreak:(id)sender;
+- (IBAction)startPage:(id)sender;
+- (IBAction)okPageDLOG:(id)sender;
+- (IBAction)cancelPage:(id)sender;
+
+- (IBAction)startComment:(id)sender;
+- (IBAction)okCommentDLOG:(id)sender;
+- (IBAction)cancelComment:(id)sender;
+
+
+    - (IBAction)startEntry:(id)sender;
+    - (IBAction)okEntryDLOG:(id)sender;
+    - (IBAction)cancelEntry:(id)sender;
+
+    - (IBAction)startSource:(id)sender;
+    - (IBAction)okSourceDLOG:(id)sender;
+    - (IBAction)cancelSource:(id)sender;
+
+    - (IBAction)cancelAction:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo;
+
 
 @end
--- BMD-X/BMDDocument.m
+++ BMD-X/BMDDocument.m
@@ -14,10 +14,126 @@
 {
     self = [super init];
     if (self) {
-        // Add your subclass-specific initialization here.
-        // If an error occurs here, send a [self release] message and return nil.
+        if (mString == nil) {
+            
+            mString = [[NSAttributedString alloc] initWithString:@""];
+            
+        }    
     }
     return self;
+}
+
+
+
+- (NSAttributedString *) string { return [[mString retain] autorelease]; }
+
+
+
+- (void) setString: (NSAttributedString *) newValue {
+    
+    if (mString != newValue) {
+        
+        if (mString) [mString release];
+        
+        mString = [newValue copy];
+        
+    }
+    
+}
+- (IBAction)sendBreak:(id)sender {
+    NSString* string = [NSString stringWithFormat:@"+BREAK\r", [pageFieldSolo stringValue]];
+    
+    [[[textView textStorage] mutableString] appendString: string];
+}
+
+- (IBAction)startPage:(id)sender{
+    [NSApp beginSheet:mPageWindow modalForWindow:self.windowForSheet modalDelegate:self didEndSelector:@selector(cancelAction:returnCode:contextInfo:) contextInfo:@"entry"];
+}
+
+- (IBAction)okPageDLOG:(id)sender{
+    NSString* string = [NSString stringWithFormat:@"+PAGE,%@\r", [pageFieldSolo stringValue]];
+    
+    [[[textView textStorage] mutableString] appendString: string];
+    [NSApp endSheet:mPageWindow returnCode:[sender tag]];
+}
+- (IBAction)cancelPage:(id)sender{
+    [NSApp endSheet:mPageWindow returnCode:[sender tag]];
+}
+
+
+
+- (IBAction)startComment:(id)sender{
+    [NSApp beginSheet:mCommentWindow modalForWindow:self.windowForSheet modalDelegate:self didEndSelector:@selector(cancelAction:returnCode:contextInfo:) contextInfo:@"entry"];
+}
+- (IBAction)okCommentDLOG:(id)sender{
+    NSString* string = [NSString stringWithFormat:@"#THEORY %@\r", [commentField stringValue]];
+
+    [[[textView textStorage] mutableString] appendString: string];
+    [NSApp endSheet:mCommentWindow returnCode:[sender tag]];
+}
+- (IBAction)cancelComment:(id)sender{
+    [NSApp endSheet:mCommentWindow returnCode:[sender tag]];
+}
+
+
+
+
+- (IBAction)okSourceDLOG:(id)sender;
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"dd MMMM yyyy"];
+    NSString* aStr = [dateFormatter stringFromDate:[dateField dateValue]];
+
+    NSMutableString* string = [NSMutableString stringWithFormat:@"+%@,%@,%@", [[[formatMenu selectedItem] title] substringToIndex:1], [yearField stringValue], [[quarterMenu selectedItem] title]];
+
+    if ( ! [[rangeField stringValue] isEqualToString:@""] )
+        [string appendString:[NSString stringWithFormat:@",%@", [rangeField stringValue]]];
+    if ( ! [[locationField stringValue] isEqualToString:@""] )
+        [string appendString:[NSString stringWithFormat:@",%@", [locationField stringValue]]];
+    if ( ! [[freeBMDRefField stringValue] isEqualToString:@""] )
+        [string appendString:[NSString stringWithFormat:@",%@", [freeBMDRefField stringValue]]];
+
+    [string appendString:[NSString stringWithFormat:@",%@\r",aStr]];
+    
+    if ( ! [[pageField stringValue] isEqualToString:@""] )
+        [string appendString:[NSString stringWithFormat:@"+PAGE,%@\r", [pageField stringValue]]];
+    
+    
+    [[[textView textStorage] mutableString] appendString: string];
+    
+    [NSApp endSheet:mSourceWindow returnCode:[sender tag]];
+}
+- (IBAction)okEntryDLOG:(id)sender
+{
+    NSString* string = [NSString stringWithFormat:@"+INFO,%@,%@,%@,%@,macintosh\r", [emailField stringValue], [passwordField stringValue], [[orderMenu selectedItem] title], [[typeMenu selectedItem] title] ];
+    
+    [[[textView textStorage] mutableString] appendString: string];
+    [NSApp endSheet:mEntryWindow returnCode:[sender tag]];
+}
+- (IBAction)cancelSource:(id)sender
+{
+    [NSApp endSheet:mSourceWindow returnCode:[sender tag]];
+}
+
+- (IBAction)cancelEntry:(id)sender
+{
+    [NSApp endSheet:mEntryWindow returnCode:[sender tag]];
+}
+
+- (IBAction)startEntry:(id)sender
+{
+	[NSApp beginSheet:mEntryWindow modalForWindow:self.windowForSheet modalDelegate:self didEndSelector:@selector(cancelAction:returnCode:contextInfo:) contextInfo:@"entry"];
+}
+- (IBAction)startSource:(id)sender
+{
+    [dateField setDateValue:[NSDate date]];
+	[NSApp beginSheet:mSourceWindow modalForWindow:self.windowForSheet modalDelegate:self didEndSelector:@selector(cancelAction:returnCode:contextInfo:) contextInfo:@"source"];
+}
+
+- (void)cancelAction:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo
+{
+    [sheet orderOut:self];
+
 }
 
 - (NSString *)windowNibName
@@ -30,29 +146,63 @@
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
     [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
-}
+    if ([self string] != nil) {
+        
+        [[textView textStorage] setAttributedString: [self string]];
+        
+    }}
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
-    /*
-     Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+
+{
+    
+    BOOL readSuccess = NO;
+    
+    NSAttributedString *fileContents = [[NSAttributedString alloc]
+                                        
+                                        initWithData:data options:NULL documentAttributes:NULL
+                                        
+                                        error:outError];
+    
+    if (fileContents) {
+        
+        readSuccess = YES;
+        
+        [self setString:fileContents];
+        
+        [fileContents release];
+        
     }
-    return nil;
+    
+    return readSuccess;
+    
 }
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
-    /*
-    Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    */
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return YES;
-}
 
+
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+
+{
+    
+    NSData *data;
+    
+    [self setString:[textView textStorage]];
+    
+    NSMutableDictionary *dict = [NSDictionary dictionaryWithObject:NSRTFTextDocumentType
+                                 
+                                                            forKey:NSDocumentTypeDocumentAttribute];
+    
+    [textView breakUndoCoalescing];
+    
+    data = [[self string] dataFromRange:NSMakeRange(0, [[self string] length])
+            
+                     documentAttributes:dict error:outError];
+    
+    return data;
+    
+}
+- (void) textDidChange: (NSNotification *) notification
+{
+    [self setString: [textView textStorage]];
+}
 @end
